0cffb8723acdf386cf66b9e416552b62ccfe83c2
LMarcin12 <mlanda2@hawk.iit.edu>
2023-10-03 23:25:09 -0500

    Sleepy Time

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,55 +10,45 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
+        <q-btn
+          flat
+          dense
+          round
+          icon="add"
+          aria-label="Add"
+          @click="prompt = true"
+        />
       </q-toolbar>
-        
-      <div class = "q-px-lg q-py-xl q-mb-md">
-        <div class="text-h3">Todo</div>
-        <div class="text-subtitle1">{{ todaysDate }}</div>
+
+      <div class="q-px-lg q-py-xl q-mb-md">
+        <div class="text-h2">
+          <center>Assignments</center>
+        </div>
+        <div class="text-subtitle1">
+          <center>({{ todaysDate }})</center>
+        </div>
       </div>
-      <q-img
-        src = "src/statics/forest.jpg"
-        class = "header-image absolute-top" />
+      <q-img src="src/statics/forest.jpg" class="header-image absolute-top" />
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
-        <q-item
-          to = "/"
-          exact
-          clickable
-          v-ripple>
+        <q-item to="/" exact clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name = "today" />
+            <q-icon name="today" />
           </q-item-section>
 
-          <q-item-section>
-            Week View
-          </q-item-section>
+          <q-item-section> Week View </q-item-section>
         </q-item>
 
-        <q-item
-          to = "/Extra"
-          exact
-          clickable
-          v-ripple>
+        <q-item to="/Extra" exact clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name = "list" />
+            <q-icon name="list" />
           </q-item-section>
 
-          <q-item-section>
-            Assignment View
-          </q-item-section>
+          <q-item-section> Assignment View </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
@@ -69,11 +59,43 @@
       </KeepAlive>
     </q-page-container>
   </q-layout>
+  <q-dialog v-model="prompt" persistent>
+    <q-card style="min-width: 350px">
+      <q-card-section>
+        <div class="text-h6">Assignment Name:</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-input
+          dense
+          v-model="assignment"
+          autofocus
+          @keyup.enter="prompt = false"
+        />
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h6">Due Date:</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-input
+          dense
+          v-model="dueDate"
+          autofocus
+          @keyup.enter="prompt = false"
+        />
+      </q-card-section>
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="Cancel" v-close-popup />
+        <q-btn flat label="Add Assignment" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import { date } from 'quasar'
+import { defineComponent, ref } from "vue";
+import { date } from "quasar";
 
 const linksList = [
   {
@@ -81,38 +103,49 @@ const linksList = [
     //title: 'Todo',
     //caption: 'List of Assignments',
     //icon: 'list',
-  }
-]
+  },
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
+      prompt: ref(false),
+      assignment: ref(""),
+      dueDate: ref(""),
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
   },
   computed: {
-    todaysDate(){
-      let timeStamp = Date.now()
-      return date.formatDate(timeStamp, 'MM/DD/YY')
-    }
-  }
-})
-
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "MM/DD/YY");
+    },
+  },
+  methods: {
+    addTask() {
+      Extra.tasks.push({
+        title: "MainLayout.title",
+        dueDate: "MainLayout.dueDate",
+        done: false,
+      });
+    },
+  },
+});
 </script>
 
-<style lang = "scss">
-  .header-image{
-    height: 100%;
-    z-index: -1;
-    opacity: 1;
-    filter: grayscale(0%);
-  }
-  //quasar dev
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 1;
+  filter: grayscale(0%);
+}
+//quasar dev
 </style>
--- src/pages/Extra.vue
+++ src/pages/Extra.vue
@@ -1,14 +1,87 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1> Extra </h1>
-  </q-page>
+  <KeepAlive>
+    <q-page class="bg-grey-3 column">
+      <q-list class="bg-white" separator bordered>
+        <q-item
+          v-for="(task, index) in tasks"
+          :key="task.title"
+          clickable
+          @click="task.done = !task.done"
+          :class="{ done: task.done }"
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-checkbox v-model="task.done" color="primary" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ task.title }}</q-item-label>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-right">{{ task.dueDate }}</q-item-label>
+          </q-item-section>
+          <q-item-section v-if="task.done" side>
+            <q-btn
+              @click.stop="deleteTask(index)"
+              push
+              color="primary"
+              icon="delete"
+              dense
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+      <div class="no-tasks" v-if="!tasks.length">
+        <center>
+          <q-icon name="check" size="100px" color="primary" />
+          <div class="text-h5 text-primary">No Tasks Remaining</div>
+        </center>
+      </div>
+    </q-page>
+  </KeepAlive>
 </template>
 
 <script>
-
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Research Project",
+          dueDate: "11/15/23",
+          done: false,
+        },
+        {
+          title: "GEM Pitch",
+          dueDate: "10/15/23",
+          done: false,
+        },
+        {
+          title: "Poo Banana Pants",
+          dueDate: "10/69/23",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+});
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #aaa;
+  }
+}
+.no-tasks {
+  opacity: 0.8;
+}
+</style>
